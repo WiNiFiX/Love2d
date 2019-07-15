f02edf5d5b8fdd2a7dc420d03157fc1fe334a1ad
Brett Lombard <brett@thymetech.co.za>
2019-07-15 13:59:55 +0200

    Shader update for crazy fast performance.

--- PixelTester/main.lua
+++ PixelTester/main.lua
@@ -1,29 +1,56 @@
 local screenX, screenY = 0, 0
-local screenImgData, screenTex
+local screenCanvas
+local shader
+local shader_time = 0
+
 function love.load()
     screenX, screenY = love.graphics.getDimensions()
-    screenImgData = love.image.newImageData(screenX, screenY, "rgba8")
-    screenTex = love.graphics.newImage(screenImgData)
+    screenCanvas = love.graphics.newCanvas(screenX, screenY)
+    
+    shader = love.graphics.newShader([[
+      // Gold Noise ©2015 dcerisano@standard3d.com 
+      //  - based on the Golden Ratio, PI and Square Root of Two
+      //  - superior distribution
+      //  - fastest noise generator function
+      //  - works with all chipsets (including low precision)
+      
+      uniform float time;
+
+      float PHI = 1.61803398874989484820459 * 00000.1; // Golden Ratio   
+      float PI  = 3.14159265358979323846264 * 00000.1; // PI
+      float SQ2 = 1.41421356237309504880169 * 10000.0; // Square Root of Two
+
+      float gold_noise(in vec2 coordinate, in float seed){
+        return fract(tan(distance(coordinate*(seed+PHI), vec2(PHI, PI)))*SQ2);
+      }
+
+      vec4 effect(vec4 color, Image tex, vec2 uv, vec2 sc)
+      {
+        return vec4(
+          gold_noise(sc, time + PHI),
+          gold_noise(sc, time + PI),
+          gold_noise(sc, time + SQ2),
+          1.0f);
+      }
+    ]])
 end
 
-local function getRandomColor()
-    r = math.random(0, 255)            
-    g = math.random(0, 255)    
-    b = math.random(0, 255)    
-    return r / 255, g / 255, b / 255, 1.0
-
-end
-
-local function fillTexture(x, y, r, g, b, a)
-    return getRandomColor()
-end
 function love.update(dt)
-    screenImgData:mapPixel(fillTexture)
-    screenTex:replacePixels(screenImgData)
+  shader_time = shader_time + dt
+  shader:send("time", shader_time)
 end
+
+local function drawPixels()
+  love.graphics.setShader(shader)
+  love.graphics.rectangle("fill", 0, 0, screenX, screenY)
+  love.graphics.setShader()
+end
+
 function love.draw()    
-    love.graphics.draw(screenTex)
-    drawFPS(10, 10)
+  screenCanvas:renderTo(drawPixels)
+  
+  love.graphics.draw(screenCanvas)
+  drawFPS(10, 10)
 end
 
 function drawFPS(x, y)
