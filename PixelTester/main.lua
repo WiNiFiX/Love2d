8703d2025c48c88926f0283eee5a198a4558c154
Brett Lombard <brett@thymetech.co.za>
2019-07-15 14:54:54 +0200

    More tweaks

--- PixelTester/main.lua
+++ PixelTester/main.lua
@@ -17,7 +17,7 @@ function love.load()
       uniform float time;
 
       float PHI = 1.61803398874989484820459 * 00000.1; // Golden Ratio   
-      float PI  = 3.14159265358979323846264 * 00000.1; // PI
+      float PI  = 22 / 7.0;                            // PI
       float SQ2 = 1.41421356237309504880169 * 10000.0; // Square Root of Two
 
       float gold_noise(in vec2 coordinate, in float seed){
